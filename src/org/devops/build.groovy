7e30965dee9eaf7f31a7ddc80bed68235b7f0ee3
xd963625627 <30467460+xd963625627@users.noreply.github.com>
2021-05-12 17:46:18 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -3,6 +3,6 @@ package org.devops
 def Build(buildType, buildShell) {
   def buildTools = ["maven":"maven3.6.3", "ant":"ANT", "gradle": "GRADLE", "npm": "node"]
   buildHome = tool buildTools[buildType]
-  println('buildHome is',buildHome)
+  println("buildHome is ${buildHome}")
   sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
