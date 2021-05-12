e9e6d5b93462e2c52eb80b7fcc3b882a1dd4c339
xd963625627 <30467460+xd963625627@users.noreply.github.com>
2021-05-12 17:49:08 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,7 +1,7 @@
 package org.devops
 // 构建类型
 def Build(buildType, buildShell) {
-  def buildTools = ["maven":"maven3.6.3", "ant":"ANT", "gradle": "GRADLE", "npm": "node"]
+  def buildTools = ["mvn":"maven3.6.3", "ant":"ANT", "gradle": "GRADLE", "npm": "node"]
   buildHome = tool buildTools[buildType]
   println("buildHome is ${buildHome}")
   sh "${buildHome}/bin/${buildType} ${buildShell}"
