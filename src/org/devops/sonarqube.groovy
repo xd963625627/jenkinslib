5c3df659fa6602cdb7abe75e3e86328cfdf8cfca
xd963625627 <30467460+xd963625627@users.noreply.github.com>
2021-05-26 17:30:21 +0800

    使用Jenkins中的sonarqube-scanner插件

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -22,3 +22,26 @@ def SonarScan(projectName, projectDesc, projectPath) {
                   -Dsonar.java.surefire.report=target/surefire-reports
     """
 }
+
+// 使用jenkins插件
+def SonarScanInJenkins(projectName, projectDesc, projectPath) {
+  // 使用jenkins指定的环境 可以不需要配置server、password、login
+  withSonarQubeEnv("sonarqube-docker") {
+    def sonarHome = "/home/software/sonar-scanner"
+    // def sonarServer = "http://192.168.1.151:9000/"
+    def sonarDate = sh returnStdout: true, script: 'date +%Y%m%d%H%M%S'
+    sonarDate = sonarDate - "\n"
+    sh """
+      ${sonarHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
+                    -Dsonar.projectName=${projectName} \
+                    -Dsonar.projectVersion=${sonarDate} \
+                    -Dsonar.ws.timeout=30 \
+                    -Dsonar.projectDescription=${projectDesc} \
+                    -Dsonar.sources=${projectPath} \
+                    -Dsonar.sourceEncoding=UTF-8 \
+                    -Dsonar.java.binaries=target/classes \
+                    -Dsonar.java.test.binaries=target/test-classes \
+                    -Dsonar.java.surefire.report=target/surefire-reports
+      """
+  }
+}
