5a5a61e31f7bc8e548d4e6ff5f4afd941ae7118b
xd963625627 <30467460+xd963625627@users.noreply.github.com>
2021-06-02 15:43:51 +0800

    Create sonarapi.groovy

---
+++ src/org/devops/sonarapi.groovy
@@ -0,0 +1,25 @@
+package org.devops
+// 封装http请求
+def HttpReq(sonarServer,reqType,reqUrl,reqBody){
+    //def gitServer = "http://192.168.3.134:17000/api/v4"
+    withCredentials([string(credentialsId: 'gitlab-token', variable: 'gitlabToken')]) {
+      result = httpRequest authentication: 'c8cfe653-c5fa-4b30-b071-4c47c2a3e8d6',
+                httpMode: reqType, 
+                contentType: "APPLICATION_JSON",
+                consoleLogResponseBody: true,
+                ignoreSslErrors: true, 
+                requestBody: reqBody,
+                url: "${sonarServer}/${reqUrl}"
+                //quiet: true
+    }
+    return result
+}
+
+// 获取Sonar质量阈状态
+def GetProjectStatus(sonarServer, projectName) {
+  apiUrl = "project_branches/list?project=${projectName}
+  response = HttpReq(sonarServer,apiUrl,'')
+  response = readJSON text: """${response.content}"""
+  println(response)
+  return response
+}
